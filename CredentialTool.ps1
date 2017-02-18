e331d9a02215cd84c9c866c7cf7aaf239a5e6c95
satoki-shiro <sato.hiro.090@gmail.com>
2017-02-19 02:33:49 +0900

    Feb 19, 2017 - Code Snippet
    Password and xml

---
+++ CredentialTool.ps1
@@ -0,0 +1,13 @@
+﻿#@1: Get Instance Password
+#$credential = Get-Credential
+#$credential.GetNetworkCredential().Password
+
+
+#@2: Get Password From password file
+$credentialFile = ".\AAA.txt"
+$credential = Get-Credential
+$credential.Password | ConvertFrom-SecureString | Set-Content $credentialFile
+$securePassword = Get-Content $credentialFile | ConvertTo-SecureString
+$BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
+$Password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($BSTR)
+
---
+++ ModifyXML.ps1
@@ -0,0 +1,13 @@
+﻿[xml]$myXML = Get-Content "C:\Users\shiro\Desktop\test.xml"
+$testlist = $myXML.testlist
+foreach($node in $testlist.node)
+{
+    foreach($content in $node.content)
+    {
+        if($content.name -eq "password")
+        {
+            $content.InnerText = "bazzword"
+        }
+    }
+}
+$myXML.Save("C:\Users\shiro\Desktop\test2.xml")
