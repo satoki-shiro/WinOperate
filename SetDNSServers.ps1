a82ca83434997ce5cc162d2cbea32f1b0452e0ef
satoki-shiro <sato.hiro.090@gmail.com>
2018-02-28 23:01:11 +0900

    test
    test

---
+++ SetDNSServers.ps1
@@ -0,0 +1,8 @@
+﻿$computer = get-content C:\azam\sl.txt
+$NICs = Get-WMIObject Win32_NetworkAdapterConfiguration -computername $computer |where{$_.IPEnabled -eq “TRUE”}
+  Foreach($NIC in $NICs) {
+
+$DNSServers = “198.102.234.125",”198.102.234.126"
+ $NIC.SetDNSServerSearchOrder($DNSServers)
+ $NIC.SetDynamicDNSRegistration(“TRUE”)
+}
