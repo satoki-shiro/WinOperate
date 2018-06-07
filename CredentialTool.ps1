390f937ab2d37b5a253e877eb1024ac1e73d981f
satoki-shiro <sato.hiro.090@gmail.com>
2018-06-07 22:10:43 +0900

    test

--- CredentialTool.ps1
+++ CredentialTool.ps1
@@ -4,10 +4,15 @@
 
 
 #@2: Get Password From password file
+$user = "administrator"
 $credentialFile = ".\AAA.txt"
-$credential = Get-Credential
+$credential = Get-Credential -Credential $user
 $credential.Password | ConvertFrom-SecureString | Set-Content $credentialFile
 $securePassword = Get-Content $credentialFile | ConvertTo-SecureString
+
+# For Using remote session (etsn $hostname -cred $cred...)
+$cred = New-Object System.Management.Automation.PsCredential $user, $securePassword
+
 $BSTR = [System.Runtime.InteropServices.Marshal]::SecureStringToBSTR($securePassword)
 $Password = [System.Runtime.InteropServices.Marshal]::PtrToStringBSTR($BSTR)
 
